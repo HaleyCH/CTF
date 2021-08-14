35f22829e96cc9c25e12608a5914796c6f4f3f3f
HaleyCH <1617335718@qq.com>
2021-08-14 21:24:03 +0800

    add some crypto program

--- crypto/caesar.py
+++ crypto/caesar.py
@@ -10,13 +10,27 @@ def getCh(ch, i):
 def getCh_allCaesar(ch,i):
     return chr(ord(ch)+i)
 
+def print_ASCII(ch):
+     s = ""
+     for c in ch:
+         s = s+ str(ord(c)) +" "
+     print("[*]:"+s)
+     ss = ""
+     css = ""
+     for c in range(len(ch)-1):
+         ss = ss + str(ord(ch[c+1])-ord(ch[c])) + " "
+         css = css + chr(ord(ch[c+1])-ord(ch[c])+ord('a')) + " "
+     print("[*]:"+ss)
+     print("[*]:"+css)
+
 text = input("[@]: Input text:")
 
 for i in range(26):
     out = ""
     j = i
     for ch in text:
-        out += getCh_allCaesar(ch,j)
+        out += getCh(ch,j)
        #  j += 1
-    print("[*] :"+out)
+    print("[+] :"+out)
+print_ASCII(text)
 print("[!]: Finish.")
